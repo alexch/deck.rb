e50961bffe01ba99e858408e751259904ca85bd9
Alex Chaffee <alex@stinky.com>
2012-01-12 08:52:09 -0800

    move app builder code into App.build; be a little smarter about root dirs

--- lib/deck/app.rb
+++ lib/deck/app.rb
@@ -1,16 +1,29 @@
 module Deck
- class App
-  def initialize root, files
-    @root, @files = root, files
-  end
-  
-  def call env
-    slides = []
-    @files.each do |file|
-      slides += Slide.from_file "#{@root}/#{file}"  
+  class App
+    def self.build app_root, slide_root, slide_files
+      Rack::Builder.app do
+        use Rack::ShowExceptions
+        use Rack::ShowStatus    
+        use Rack::Static, :urls => ["/deck"], :root => app_root
+        slide_files.each do |slide_file|
+          slide_dir = File.dirname slide_file
+          use Rack::Static, :urls => ["/img"], :root => slide_dir
+        end
+        run ::Deck::App.new(slide_root, slide_files)
+      end
     end
-    deck = Deck.new :slides => slides
-    [200, {}, deck.to_pretty]
+
+    def initialize slide_root, slide_files
+      @slide_root, @slide_files = slide_root, slide_files
+    end
+
+    def call env
+      slides = []
+      @slide_files.each do |file|
+        slides += Slide.from_file "#{@slide_root}/#{file}"  
+      end
+      deck = Deck.new :slides => slides
+      [200, {}, deck.to_pretty]
+    end
   end
- end
 end
