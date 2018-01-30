e80dac5f28d3fecca91a1f1ae9cc6fa8cf1a2afb
Joshua Burke <burke.joshua.james@gmail.com>
2018-01-30 13:51:19 -0500

    Allow 'should' and 'expect' usage in expectations and mocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,12 @@ require "files"
 
 require "deck/noko"
 include Deck::Noko
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = [:should, :expect]
+  end
+end
